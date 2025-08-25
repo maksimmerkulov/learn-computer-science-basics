063c6737ca6ce0679c8c99a0817ed3a63eaac6da
maksimmerkulov <merkulov.mv@outlook.com>
2025-08-25 09:39:57 +0500

    Data Types and Algorithms. Numbers

---
+++ src/main/java/com/epam/learn/numbers/DoubleDemo.java
@@ -0,0 +1,10 @@
+package com.epam.learn.numbers;
+
+public class DoubleDemo {
+    public static void main(String[] args) {
+        double speed = 1;
+        double divide = 3;
+
+        System.out.println(speed / divide);
+    }
+}
