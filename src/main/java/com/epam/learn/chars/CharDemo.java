ff7fb2520b42229753e34a6c0ecf7131f2eb0019
maksimmerkulov <merkulov.mv@outlook.com>
2025-08-25 09:44:43 +0500

    Data Types and Algorithms. Char and String

---
+++ src/main/java/com/epam/learn/chars/CharDemo.java
@@ -0,0 +1,9 @@
+package com.epam.learn.chars;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char letter = 'h';
+
+        System.out.println(letter);
+    }
+}
---
+++ src/main/java/com/epam/learn/strings/StringDemo.java
@@ -0,0 +1,9 @@
+package com.epam.learn.strings;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String text = "hello";
+
+        System.out.println(text);
+    }
+}
