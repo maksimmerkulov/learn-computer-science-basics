95a1f3c4210ac7c5a1174aaedd862f560abc7a90
maksimmerkulov <merkulov.mv@outlook.com>
2025-08-25 09:40:42 +0500

    Data Types and Algorithms. Arrays

---
+++ src/main/java/com/epam/learn/arrays/ArrayDemo.java
@@ -0,0 +1,10 @@
+package com.epam.learn.arrays;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] array = {2, 9, 9, 7, 9, 2, 4, 5, 8};
+        array[0] = 56;
+
+        System.out.println(array[0]);
+    }
+}
