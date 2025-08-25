9ad3902d2ae3bc1e8c777965fd01d7acd2291eee
maksimmerkulov <merkulov.mv@outlook.com>
2025-08-25 09:39:09 +0500

    Data Types and Algorithms. Data Types

--- src/main/java/com/epam/learn/Main.java
+++ src/main/java/com/epam/learn/Main.java
@@ -2,6 +2,9 @@ package com.epam.learn;
 
 public class Main {
     public static void main(String[] args) {
+        int number = 7;
+
         System.out.println("hello from Java");
+        System.out.println(number);
     }
 }
